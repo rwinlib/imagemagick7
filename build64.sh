5950f0ae4a02cc53c49f242227fbce390a163746
Jeroen Ooms <jeroenooms@gmail.com>
2016-08-11 20:38:32 +0200

    first build

---
+++ build64.sh
@@ -0,0 +1,46 @@
+# Using patched imagemagick sources from msys2
+# Link against dependency C libs from msys2
+# Notes: https://github.com/Alexpux/MINGW-packages/issues/1534
+export PKG_CONFIG="/c/msys2-x64/mingw64/bin/pkg-config"
+export PKG_CONFIG_PATH="/c/msys2-x64/mingw64/lib/pkgconfig"
+export CPPFLAGS="-m64 -I/c/msys2-x64/mingw64/include -DLIBXML_STATIC -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION"
+export CFLAGS="-m64 -I/c/msys2-x64/mingw64/include -DLIBXML_STATIC -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION"
+export CXXLAGS="-m64 -I/c/msys2-x64/mingw64/include -DLIBXML_STATIC -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION"
+export LDFLAGS="-m64 -L/c/msys2-x64/mingw64/lib"
+export LIBS="-lOleAut32 -pthread -lws2_32"
+  ../ImageMagick-7.0.2-7/configure \
+    --enable-zero-configuration \
+    --enable-silent-rules \
+    --disable-shared \
+    --enable-static \
+    --enable-opencl \
+    --with-magick-plus-plus \
+    --with-rsvg \
+    --with-webp \
+    --with-zlib \
+    --with-pango \
+    --with-xml \
+    --with-freetype \
+    --with-lzma \
+    --with-png \
+    --with-fftw \
+    --with-gslib \
+    --with-lcms \
+    --without-fontconfig \
+    --without-lqr \
+    --without-wmf \
+    --without-openexr \
+    --without-openjp2 \
+    --without-gvc \
+    --without-raqm \
+    --without-djvu \
+    --without-jbig \
+    --without-modules \
+    --without-perl \
+    --without-x \
+    --without-threads \
+    --disable-deprecated \
+    --disable-dependency-tracking \
+    --disable-legacy-support \
+    --disable-hdri \
+    --with-windows-font-dir=c:/Windows/fonts
